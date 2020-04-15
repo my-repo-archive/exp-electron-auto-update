d7e7449f1ba4406d4750134844da4e4e06201ff7
Ken <2770219+ken0x0a@users.noreply.github.com>
2020-04-16 08:27:31 +0900

    chore: bkp
    draggable doesn't work...

--- dist/main.js
+++ dist/main.js
@@ -14,7 +14,9 @@ function createWindow() {
     },
     ...(process.platform === "darwin"
       ? {
-          titleBarStyle: "hidden",
+          // titleBarStyle: "hidden",
+          // vibrancy: 'under-window',
+          // frame: false,
         }
       : {
           frame: false,
@@ -22,6 +24,8 @@ function createWindow() {
         }),
   }).on("ready-to-show", (e, ...args) => {
     console.log({ e, args });
+    if (process.env.NODE_ENV === 'development')
+    mainWindow.webContents.toggleDevTools(); 
   });
 
   // and load the index.html of the app.
@@ -40,13 +44,15 @@ app.whenReady().then(createWindow);
 app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") app.quit();
+  // if (process.platform !== "darwin") app.quit();
+  app.quit();
 });
 
 app.on("activate", function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  
 });
 
 // In this file you can include the rest of your app's specific main process
